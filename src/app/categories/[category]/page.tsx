01d95bd6284dc9d204e1a8225b1b3c151f1ee920
Deepak Kataria <102903632+katariad@users.noreply.github.com>
2025-05-26 19:45:01 +0530

    Update page.tsx

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -41,9 +41,9 @@ export default async function CategoryPage({
               {<Datefunction date={post.updated_at} />}
             </span>
             <p className="text-gray-600">{post.Description}</p>
-            <Link rel="stylesheet" href={post.slug} className="readmore_button">
+            <a href={post.slug} className="readmore_button">
               Read more
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
